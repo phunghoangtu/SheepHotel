5c592d24de334c22a8115ff99dbd8cbcdefb2999
phunghoangtu <tuphph311200@gmail.com>
2024-03-09 12:55:11 +0700

    create project

---
+++ src/main/java/com/example/sheephotel/SheepHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.sheephotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheepHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheepHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sheephotel/SheepHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sheephotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheepHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
